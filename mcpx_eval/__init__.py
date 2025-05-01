81733b14d015760d6fc9c6a472f351c94cd773cc
zach <zach@dylibso.com>
2025-05-01 12:04:01 -0700

    cleanup: export ModelConfig

--- mcpx_eval/__init__.py
+++ mcpx_eval/__init__.py
@@ -1,9 +1,9 @@
 import logging
 
-from .models import Score, Results, Model, Test
+from .models import Score, Results, Model, Test, ModelConfig
 from .database import Database
 from .judge import Judge
 
 logger = logging.getLogger(__name__)
 
-__all__ = ["Score", "Results", "Model", "Test", "Database", "Judge"]
+__all__ = ["Score", "Results", "Model", "Test", "Database", "Judge", "ModelConfig"]
